97477be58142e2d71a805ebb11f2715880837ef5
Irongorilla <30632028+irongorrila@users.noreply.github.com>
2020-06-18 17:53:35 +0530

    Update Comcast_project.py

--- Comcast_project.py
+++ Comcast_project.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # Setting style for uniform formatting across project
 sns.set_style("whitegrid")
@@ -24,10 +25,6 @@ Status: Status of complaint
 Filing on behalf of someone
 Analysis Task
 
-Task 3:
-Which state has the maximum complaints
-Which state has the highest percentage of unresolved complaints
-- Provide the percentage of complaints resolved till date, which were received through the Internet and customer care calls.
 
 The analysis results to be provided with insights wherever applicable.
 
@@ -50,16 +47,16 @@ Task 1b. Provide the trend chart for the number of complaints at monthly
 sns.countplot(x= 'month', data= df)
 plt.xlabel('Months')
 plt.ylabel('Volume')
-sns.set_title(" Complaints by month")
+plt.title(" Complaints by month")
 plt.show()
-plt.clf()
+
 
 # By Date
 fig, ax=subplots()
 ax=sns.countplot(x= 'Date', data= df)
 plt.xlabel("Date")
 plt.ylabel("Volume")
-plt.title()
+plt.title("Complaints Volume by Date")
 plt.xticks(rotation=90) # too many tick labels cannot fit in graph
 plt.show()
 #########################################################################
@@ -80,6 +77,11 @@ b. Create a new categorical variable with value as Open and Closed.
 
 c. Provide state wise status of complaints in a stacked bar chart. Use the categorized 
    variable from Q3. Provide insights on:
+   
+Which state has the maximum complaints
+Which state has the highest percentage of unresolved complaints
+- Provide the percentage of complaints resolved till date, which were received through the Internet and customer care calls.
+
 
 """
 # Task 2a:
@@ -89,18 +91,40 @@ top_complaints[0:10]
 
 #######################################################
 
-# Task 2b:
-a= ['Open', 'Pending']
-df['Status_revised'] = []
+# Task 2b: Create a new categorical variable with 'Open' & 'Closed' as the statuses
 
-x= df['Status'].isin(a)
+df['new_status'] = np.nan
 
-for i in range(len(x)):
-    if x[i] == True:
-        df['Status_revised'][i] = 'Open'
-    else:
-        df['Status_revised'][i] = 'Closed'
+for i in range(len(df)):
+   if ((df['Status'][i] == 'Open') or (df['Status'][i] == "Pending")):
+      df['new_status'][i] = "Open"
+   else:
+      df['new_status'][i] = "Closed"
 
 
-   
+# Task 2c:
+
+df_sub= df[['State', 'new_status']]; df_sub.head()
+state_wise= pd.DataFrame(df_sub.groupby(['State', 'new_status']).agg(np.size))
+state_wise.unstack().plot(kind= 'bar', stacked= True, label= ['Open', 'Closed'])
+plt.title("State-wise Status of Cases")
+plt.legend()
+plt.show()
+
+sw_un= state_wise.head().unstack()
+closed_cases= np.array(sw_un[0, 'Closed'])
+open_cases= np.array(sw_un[0, 'Open'])
+sw_un["Total"] = closed_cases + open_cases
+
+sw_un['perc_unresolved'] = open_cases/sw_un["Total"]
+sw_un['perc_resolved'] = closed_cases/sw_un["Total"]
+
+top_unresolved= sw_un.sort_values('perc_unresolved', ascending= False)
+top_resolved= sw_un.sort_values('perc_resolved', ascending= False)
+
+# Subsetting cases from Alabama which were received via Customer Care or Internet
+df_via= df[(df['Received Via'].isin(['Customer Care Call', 'Internet'])) & (df['State'] == "Alabama")]
+
+df_via['new_status'].value_counts(normalize= True)
+
 
